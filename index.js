4cd4709bbdc67dff24264b687f3cea10f692982d
Kairos22 <alandiko84@gmail.com>
2022-08-22 14:14:06 +0200

    arraymethod-filter

--- index.js
+++ index.js
@@ -2,21 +2,22 @@
 //1
 
 function filterRange(arr, a, b) {
-    //write your code here
+    return arr.filter(item => (item >= a && item <= b));
+
   }
   
   let arr = [5, 3, 8, 1];
   
   let filtered = filterRange(arr, 1, 4);
   
-  console.log( filtered ); // 3,1 (matching values)
+  console.log(filtered); // 3,1 (matching values)
   
-  console.log( arr ); // 5,3,8,1 (not modified)
+  console.log(arr); // 5,3,8,1 (not modified)
 
 //Write a function filterRangeInPlace(arr, a, b) that gets an array arr and removes from it all values except those that are between a and b. The test is: a ≤ arr[i] ≤ b.
 //2
   function filterRangeInPlace(arr, a, b) {
-    //write your code here
+     
   }
   
   let array = [5, 3, 8, 1];
